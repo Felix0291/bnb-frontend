d8e94c3e8228ff61a12bd0ebfbb413849ac1abea
Felix0291 <felix.tapper@gmail.com>
2025-10-28 11:44:36 +0100

    Link navigerar till / sidan när man tycker på loggan

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 import OpenHouseImg from "../assets/openHouseImg.png"
 import { TfiAlignJustify } from "react-icons/tfi";
+import { Link } from "react-router-dom";
+
 
 
 
 
 const Header = () => {
-    // const [showMenu, setShowMenu] = useState(false);
-
-    // const toggleMenu = () => {
-    //     setShowMenu(prev => !prev);
-    // };
+    const [loggedIn, setLoggedIn] = useState(false)
     return (
-        
+
         <div className="absolute top-0 left-0 right-0 w-full h-20 bg-lightblue text-black font-mono p-4 z-50 border-b-2 border-lightgrey flex items-center">
-    <img className="w-[80px] rounded-xl ml-4" src={OpenHouseImg} alt="" />   
-    
-</div>
+            <Link to = "/">
+            <img className="w-[80px] rounded-xl ml-4" src={OpenHouseImg} alt="" />
+            </Link>
+        </div>
+        
     );
 };
 export default Header;
