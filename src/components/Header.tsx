b050e334314b6cb6b7aa6e9f724a31f50d8cbcc6
Felix0291 <felix.tapper@gmail.com>
2025-10-28 11:21:12 +0100

    logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
+import OpenHouseImg from "../assets/openHouseImg.png"
 import { TfiAlignJustify } from "react-icons/tfi";
 
 
 
 
 const Header = () => {
-    const [showMenu, setShowMenu] = useState(false);
+    // const [showMenu, setShowMenu] = useState(false);
 
-    const toggleMenu = () => {
-        setShowMenu(prev => !prev);
-    };
+    // const toggleMenu = () => {
+    //     setShowMenu(prev => !prev);
+    // };
     return (
-        <div className="absolute top-0 left-0 right-0 w-full h-12 bg-lightblue text-black font-mono p-4 z-50 border-b-2 border-black flex items-center justify-center">
-            
-            <h3 className="text-black font-mono bg-transparent flex-shrink-0">Oppendoors BNB</h3>
-            
-            </div>
+        
+        <div className="absolute top-0 left-0 right-0 w-full h-20 bg-lightblue text-black font-mono p-4 z-50 border-b-2 border-lightgrey flex items-center">
+    <img className="w-[80px] rounded-xl ml-4" src={OpenHouseImg} alt="" />   
+    
+</div>
     );
 };
 export default Header;
