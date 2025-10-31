040b4b73b2c7486e745e5239c965f370f657e92f
Felix0291 <felix.tapper@gmail.com>
2025-10-31 15:19:31 +0100

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ import UserMenu from "./UserMenu";
 const Header = () => {
     return (
 
-        <div className="absolute top-0 left-0 right-0 w-full h-20 bg-gray-200 text-black font-mono p-4 z-50 flex items-center">
+        <div className="fixed top-0 left-0 right-0 w-full h-20 bg-gray-200 text-black font-mono p-4 z-50 flex items-center">
             <Link to="/">
             <img className="w-[80px] rounded-xl ml-4" src={OpenHouseImg} alt="" />
             </Link>
